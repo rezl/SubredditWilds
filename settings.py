cac1952a32a97d3eb8cb704aa1207706db2f864a
Nommabelle Reddit <greatergardevoir@gmail.com>
2022-11-12 20:48:04 +0000

    Add wiki to mod perms

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ class Settings:
     is_dry_run = False
     post_check_frequency_mins = 1
     # comment mods are mods with, and only with, these permissions
-    comment_mod_permissions = ["posts", "mail"]
+    comment_mod_permissions = ["posts", "mail", "wiki"]
     comment_mod_whitelist = ["CollapseBot", "StatementBot", "CollapseTesting"]
